38089e9be9fc5330abdf253c7be0f985874761e1
Phu Nguyen <thienpn1704@gmail.com>
2022-10-03 13:32:06 -0500

    Create a function to get computer choice

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ function randomNumber () {
     let rand = Math.floor(Math.random()*3);
     return rand;
 }
+
+function getComputerChoice () {
+    const choices = ["rock", "paper", "scissors"];
+    let computerChoice = choices[randomNumber()];
+    return computerChoice
+}
+
+console.log(getComputerChoice());
